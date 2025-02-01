a5a74375e00bf75aa04152b16296a509be42d41a
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-02-01 16:26:59 +0500

    buy delete

--- input.js
+++ input.js
@@ -21,7 +21,6 @@ function showData(arr) {
           <h3>Chegirmada :${e.price - 9000.1} so'm</h3>
           <button onClick=deleteProduct(${e.id})>DELETE</button>
           <button onClick=updateProduct(${e.id})>UPDATE</button>
-          <button onClick= buyProduct(${e.id})>BUY</button>
         </div>`
     );
   });
